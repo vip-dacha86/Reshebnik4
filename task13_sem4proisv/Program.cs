7f84af3e08cf29526c9e56358b0865d98f4c9e96
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-22 14:55:36 +0500

    task13_sem4proisv

---
+++ task13_sem4proisv/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая принимет на вход число N и выдаетпроизведение чисел от 1 до N.
+// 4->24  5->120=1*2*3*4*5
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр = {GetProizv(num)}");
+int GetProizv(int n)
+{
+    int result = 1;
+    for (int i =2; i <= n; i++)
+    {
+    result*=i;
+    }
+    return result;
+    
+}
