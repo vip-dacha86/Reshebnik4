9ee5fbf5f433f97570c66a9715894c1508942327
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-22 10:44:18 +0500

    task11_sem4sum

---
+++ task11sem4sum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая принимает на вход число А и выдает сумму чисел от 1 до А.
+// а =5  sum =1+2+3+4+5
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+// int a = GetSum(n);
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
+int GetSum(int n){
+    int sum = 0;
+    for (int i = 1;i<= n; i++)
+    sum += i;// sum =sum+1
+    return sum;
+
+}
+
