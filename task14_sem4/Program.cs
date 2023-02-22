8a162f0e9bc1ee32d7d2b6bf382189429f276dfa
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-22 16:16:51 +0500

    task14_sem4massiv

---
+++ task14_sem4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу которая выводит массив из 8 элементов заполненный 0 и 1 в случайном порядке.
+
+
+
+
+int[] array = GetBinaryArray(8);
+// Console.WriteLine($"[     {String.Join(",   ", array)}   ]");
+
+ for (int i = 0;i < array.Length -1;i++){
+     Console.Write($"{array[i]},  ");
+ }
+    Console.WriteLine($"  {array[array.Length-1]} "); 
+    int[] GetBinaryArray(int size){
+        int[]result = new int[size];
+        for(int i = 0;i<size;i++)
+        {
+            result [i] = new Random().Next(2);
+        }
+    return result;
+    }
